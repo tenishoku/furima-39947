13eba7b400febf3e350f684d39481b886399c0ea
tenishoku <148612755+tenishoku@users.noreply.github.com>
2024-01-17 18:34:06 +0900

    (修正2) #4 ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
     validates :last_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'is invalid. Input full-width katakana characters' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' } do
     validates :first_name_kana
     validates :last_name_kana
   end
