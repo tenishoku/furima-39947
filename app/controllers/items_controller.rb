3d747f9842dc0bedbac2c416c3254d523b389244
tenishoku <148612755+tenishoku@users.noreply.github.com>
2024-01-18 21:11:51 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,12 @@ class ItemsController < ApplicationController
   end
 
   def create
+    Item.create(item_params)
+    redirect_to '/'
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
   end
 end
